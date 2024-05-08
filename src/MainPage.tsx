11d59df17dfd1bd2f0cc8a0031631e5803393ea8
Martin Marinov <martinmarinov150402@gmail.com>
2024-05-08 15:42:22 +0300

    Login Modal Added

--- src/MainPage.tsx
+++ src/MainPage.tsx
@@ -2,12 +2,14 @@ import React from 'react';
 import './MainPage.css';
 import NavBar from './components/NavBar';
 import { ConfigurationDesc } from './components/ConfigurationDesc';
+import { LoginModal } from './components/LoginModal';
 
 function MainPage() {
   return (
     <>
       <NavBar></NavBar>
       <ConfigurationDesc></ConfigurationDesc>
+      <LoginModal></LoginModal>
     </>
   );
 }
--- src/components/LoginModal.tsx
+++ src/components/LoginModal.tsx
@@ -1,10 +1,56 @@
-export function LoginModal() {
-    return (
-        <div className = "w-1/3">
-            <form>
+import { Controller, useForm } from "react-hook-form"
+import { Input } from "@material-tailwind/react";
 
-                
-            </form>
+export function LoginModal() {
+
+    const {
+    control,
+    handleSubmit,
+    formState: { errors },
+  } = useForm({
+    defaultValues: {
+      username: "",
+      password: "",
+    },
+  })
+    return (
+        <div className = "w-1/3 h-96 fixed bg-zinc-700 items-center border-2 border-solid border-indigo-600 flex flex-col">
+            <h1 className="text-white font text-3xl mt-5">Sign in to the site</h1>
+            <Controller
+                control={control}
+                render={({ field: { onChange, onBlur, value } }) => (
+                    <div className = "w-2/3 mt-6">
+                        <input
+                        placeholder="Username"
+                        onBlur={onBlur}
+                        onChange={onChange}
+                        value={value}
+                        type="text"
+                        className="bg-white"
+                        />
+                    </div>
+                  )}
+                name="username"
+            />
+
+            <Controller
+                control={control}
+                render={({ field: { onChange, onBlur, value } }) => (
+                    <div className="w-2/3">
+                        <input
+                        mt-5
+                        placeholder="Password"
+                        onBlur={onBlur}
+                        onChange={onChange}
+                        value={value}
+                        type = "password"
+                        />
+                    </div>
+                  )}
+                name="password"
+            />
+
+            <button className = "w-1/3 purple accent-5 mt-5 h-12 text-white" onClick={handleSubmit(data => console.log(data))}>Login</button>        
         </div>
     )
 }
